4cca84e433193223b020cc77ce38b9b5f880a49a
Hedius <admin@hedius.at>
2020-02-09 16:04:34 +0100

    v1.0 - optimized code
    Signed-off-by: Hedius <admin@hedius.at>

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# E4GL BF4Status Discord Bot
+# E4GL BF4 Status Discord Bot
 This docker image allows you to display the player count of your  BF4 servers on discord.
+
+This image extends Potato's original discord bot.
 
 ## Installation
 ### docker-compose
  1. clone the repository
- 2. copy discordbot.cfg.example to discordbot.cfg and edit it
+ 2. adjust the config - Repeat the following steps for each server
+    1. `cp discordbot.cfg.example discordbot_server1.cfg`
+    2. edit the config
+        * bot token and server guid needed
+    3. create a own service for each bot in the docker-compe.yml:w
  3. sudo docker-compose up -d
 
 ## Updating
--- src/E4GL_BF4Status_DiscordBot.py
+++ src/E4GL_BF4Status_DiscordBot.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
-"""
-Copyright (C) 2019 Hedius gitlab.com/hedius
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 __author__ = "Hedius"
-__version__ = "1.0"
+__version__ = "1.0.0"
 __license__ = "GPLv3"
-__status__ = "Production"
+
+#  Copyright (C) 2020. Hedius gitlab.com/hedius
+#  Copyright (C) 2020. Potato for the example
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
@@ -32,58 +45,66 @@ import aiohttp
 import discord
 
 
-class LivePlayercountBot(discord.Client):
-    """Discord bot to display the BL true playercount in the bot status"""
+class BF4StatusBot(discord.Client):
+    """Discord bot to display the true playercount as a bot status"""
+
+    def __init__(self, server_guid: str):
+        super().__init__()
+        self.server_guid = server_guid
+
     async def on_ready(self):
-        logging.info(f"Logged on as {self.user}")
         status = ""
         async with aiohttp.ClientSession() as session:
             while True:
-                newstatus, presence_status = await get_playercount(session)
-                if newstatus != status:
-                    await self.change_presence(activity=discord.Game(newstatus),
+                new_status, presence_status = await self.get_playercount(session)
+                if new_status != status:
+                    await self.change_presence(activity=discord.Game(new_status),
                                                status=presence_status)
-                    status = newstatus
-                await asyncio.sleep(10)
+                    status = new_status
+                await asyncio.sleep(30)
 
+    async def get_playercount(self, session):
+        """Return the current content for the given presence
+        :param session: aoihttp session
+        :return: status, and presence_status (Online, AFK, DND)
+        """
+        try:
+            url = f"http://battlelog.battlefield.com/bf4/servers/show/pc/{self.server_guid}/?json=1&join=false"
+            async with session.get(url) as r:
+                page = await r.json()
+                try:
+                    max_slots = page["message"]["SERVER_INFO"]["slots"]["2"]["max"]
+                    true_playercount = len(page["message"]["SERVER_PLAYERS"])
+                except TypeError:
+                    logging.exception("Server offline")
+                    return f"offline", discord.Status.dnd
 
-async def get_playercount(session):
-    """Get the current playerdata from BL"""
-    try:
-        url = f"http://battlelog.battlefield.com/bf4/servers/show/pc/{SERVER_GUID}/?json=1&join=false"
-        async with session.get(url) as r:
-            page = await r.json()
-            try:
-                max_slots = page["message"]["SERVER_INFO"]["slots"]["2"]["max"]
-                true_playercount = len(page["message"]["SERVER_PLAYERS"])
-            except TypeError:
-                logging.exception("Server offline")
-                return f"offline", discord.Status.dnd
+                true_playercount = max_slots if true_playercount >= max_slots else true_playercount
 
-            true_playercount = (max_slots) if true_playercount >= max_slots else true_playercount
-
-            status = discord.Status.idle if true_playercount >= 16 else discord.Status.dnd
-            if true_playercount > 32:
-                status = discord.Status.online
-
-            return f"{true_playercount}/{max_slots} players online", status
-    except Exception:
-        logging.exception("Error getting data from battlelog")  # BL autism
+                if true_playercount / max_slots >= 0.6:
+                    status = discord.Status.online
+                elif true_playercount / max_slots >= 0.35:
+                    status = discord.Status.idle
+                else:
+                    status = discord.Status.dnd
+                return f"{true_playercount}/{max_slots} players online", status
+        except Exception:
+            logging.exception("Error getting data from battlelog")
 
 
 # Config/Argv
-def readConfig(configFile):
-    """reads the config
-    :param configFile: path to config file
-    :returns: bot secret, server guid
+def read_config(config_file):
+    """Read the config
+    :param config_file: path to config file
+    :return: bot secret, server guid
     """
     # read config
-    if not os.path.isfile(configFile):
+    if not os.path.isfile(config_file):
         print("Error while accessing config!", file=sys.stderr)
         exit(1)
 
     config = configparser.ConfigParser(interpolation=None)
-    config.read(configFile)
+    config.read(config_file)
     # data
     try:
         section = config["BOT"]
@@ -101,18 +122,16 @@ def main():
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s]: %(message)s"
     )
-    global BOT_TOKEN
-    global SERVER_GUID
 
-    parser = argparse.ArgumentParser(description="Discord Bot for displaying"
-                                     "status of BF4 servers by E4GL")
+    parser = argparse.ArgumentParser(description="Discord bot for displaying"
+                                                 "the status of BF4 servers on Discord")
     parser.add_argument("-c", "--config", help="path to config file",
                         required=True, dest="configFile")
     args = parser.parse_args()
 
-    BOT_TOKEN, SERVER_GUID = readConfig(args.configFile)
+    bot_token, server_guid = read_config(args.configFile)
 
-    LivePlayercountBot().run(BOT_TOKEN)
+    BF4StatusBot(server_guid).run(bot_token)
 
 
 if __name__ == "__main__":
